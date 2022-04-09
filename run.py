fac58a2a60f49cae022669c08e5f13bada3459f5
zcf0508 <zcf0508@live.com>
2022-04-09 16:57:46 +0800

    U 补充测试用例

--- run.py
+++ run.py
@@ -1,8 +1,112 @@
 #!/usr/bin/python3
 
+import sys
+import threading
 import time
 from Vue.Proxy import Proxy, effect
 from Vue import computed, watch
+
+
+def test1():
+  print('--test1--')
+
+  data = {'text': 'hello world'}
+
+  obj = Proxy(data)
+
+  def effect_lambda():
+    print(obj['text'])
+
+  effect(effect_lambda)
+
+  time.sleep(1)
+
+  obj['text'] = 'hello pyvue'
+
+
+def test2():
+  print('--test2--')
+
+  data = {'ok': False, 'text': 'hello world'}
+
+  obj = Proxy(data)
+
+  def effect_lambda():
+    print(obj['text'] if obj['ok'] else 'not')
+
+  effect(effect_lambda)
+
+  obj['text'] = 'hello pyvue'
+
+
+def test3():
+  print('--test3--')
+
+  data = {'foo': 1}
+
+  obj = Proxy(data)
+
+  def effect_lambda():
+    obj['foo'] += 1  # 递归
+
+  effect(effect_lambda)
+
+
+def test4():
+  print('--test4--')
+
+  data = {'foo': 1}
+
+  obj = Proxy(data)
+
+  def effect_lambda():
+    print(obj['foo'])
+
+  global job_queue
+  global threads
+  global is_flushing
+
+  job_queue = set()  # 定义一个任务队列
+  threads = []
+  is_flushing = False  # 一个标志代表是否正在刷新队列
+
+  def flush_job():
+    global job_queue
+    global threads
+    global is_flushing
+
+    # 如果队列正在刷新，则什么都不做
+    if is_flushing: return
+
+    # 设置为 True 表示正在刷新
+    is_flushing = True
+
+    for job in iter(job_queue):
+
+      def task(job):
+        import time
+        time.sleep(sys.float_info.min)
+        job()
+
+      t = threading.Thread(target=task(job))
+      threads.append(t)
+      t.start()
+
+    for t in threads:
+      t.join()
+
+    # 结束后重置 is_flushing
+    is_flushing = False
+
+  def scheduler_lambda(fn):
+    global job_queue
+    job_queue.add(fn)
+    threading.Thread(target=flush_job).start()
+
+  effect(effect_lambda, {'scheduler': scheduler_lambda})
+
+  obj['foo'] += 1
+  obj['foo'] += 1
 
 
 def test_computed():
@@ -73,6 +177,10 @@ def test_watch():
 
 
 def main():
+  test1()
+  test2()
+  test3()
+  test4()
   test_computed()
   test_watch()
 
